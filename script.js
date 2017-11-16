5a73c12cf7bd58f38fae08f50347ca061e22aa57
Seweryn Michalewski <severyn.m@gmail.com>
2017-11-16 21:54:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,81 @@
+//1 stwotiujemo zapyt pytan za dopomohoju masywu i funkcji
+//stwotworiujemo masyw dla wywodu widpowidej
+var answer = new Array()
+//masyw w jakomu majemo zapytannia
+var question = ["Please enter your name", "Please enter event which one you like to visit", "Please enter time", "Please enter place"]
 
-//1
-var name = prompt("Please enter your name");
-var time = prompt("Please enter time");
-var place = prompt("Please enter place");
-var event = prompt("Please enter event which one you like to visit");
+
+//функція якою формуємо вивід відповіді
+function wyklyk() {
+    for (var i = 0; i < question.length; i++) {
+        var zapyt = prompt(question[i]);
+        answer.push(zapyt);
+    };
+};
+//запускаємо програму
+wyklyk();
 
 var catEl = document.createElement("p");
-     style = catEl.style
-     
-     style.color = "blue";
-     style.display = "flex";
-     style.justifyContent ="center";
-     style.alignItems = "center";
-     style.padding = "10px";
+style = catEl.style
 
-      catEl.innerHTML = name + " wants to visit " + event + " about " + time + " at the " + place ;
-     
+style.color = "blue";
+style.display = "flex";
+style.justifyContent = "center";
+style.alignItems = "center";
+style.padding = "10px";
 
+catEl.innerHTML = answer[0] + " wants to visit " + answer[1] + " about " + answer[2] + " at the " + answer[3];
 
 var div = document.createElement("div");
-    
-      style = div.style
-    
-        style.border = "5px solid black";
-        style.width = "auto";
-        style.height = "auto";
-        style.position = "absolute";
-        style.left = "35%";
-        style.top = "20%";
-        style.justifyContent ="center";
-        style.alignItems = "center";
-       
-     
-     div.appendChild(catEl);
-     document.body.appendChild(div);
+
+style = div.style
+
+style.border = "5px solid black";
+style.width = "auto";
+style.height = "auto";
+style.position = "absolute";
+style.left = "35%";
+style.top = "20%";
+style.justifyContent = "center";
+style.alignItems = "center";
+
+
+div.appendChild(catEl);
+document.body.appendChild(div);
 
 //2 
 var euro = prompt("EURO");
-    
-    euro = euro;
+
+euro = euro;
 
 var dolar = prompt("DOLAR");
 
-    dolar = dolar;
+dolar = dolar;
 
 
 var valuta = document.createElement("p");
-     style = valuta.style
-     style.color = "red";
-     style.display = "flex";
-     style.justifyContent ="center";
-     style.alignItems = "center";
-     style.padding = "10px";
+style = valuta.style
+style.color = "red";
+style.display = "flex";
+style.justifyContent = "center";
+style.alignItems = "center";
+style.padding = "10px";
 
-     valuta.innerHTML = euro + " euros are equal " + euro * 30.23 + " grn, " + dolar + " dollars are equal " + dolar * 26.10 + " grn, " +
-      " one euro is equal 1.074 dollars. ";
-     
+valuta.innerHTML = euro + " euros are equal " + euro * 30.23 + " grn, " + dolar + " dollars are equal " + dolar * 26.10 + " grn, " +
+    " one euro is equal 1.074 dollars. ";
+
 
 
 var block = document.createElement("block");
-     
-     style = block.style
-    
-      style.border = "5px solid black";
-      style.width = "auto";
-      style.height = "auto";
-      style.position = "absolute";
-      style.left = "35%";
-      style.top = "32%";
- 
- block.appendChild(valuta);
-     document.body.appendChild(block);
+
+style = block.style
+
+style.border = "5px solid black";
+style.width = "auto";
+style.height = "auto";
+style.position = "absolute";
+style.left = "35%";
+style.top = "50%";
+
+block.appendChild(valuta);
+document.body.appendChild(block);
